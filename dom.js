550205da00d97a49c923cdcc446e9b0875c88132
Rakshanda B.Wankhade <70371110+rakshanda1234@users.noreply.github.com>
2022-07-23 23:56:19 +0530

    Add files via upload
    task-5
    GetElementByTagName

--- dom.js
+++ dom.js
@@ -18,26 +18,60 @@ console.log(document.images);
 
 //GET ELEMENT BY ID//
 //console.log(document.getElementById("header-title"));
-var headerTitle = document.getElementById("header-title");
-var header = document.getElementById("main-header");
+//var headerTitle = document.getElementById("header-title");
+//var header = document.getElementById("main-header");
 
-console.log(headerTitle);
+//console.log(headerTitle);
 //headerTitle.textContent = "Hello";
 //headerTitle.innerText = "Goodbye";
 //console.log(headerTitle.textContent);
 //console.log(headerTitle.innerText);
-headerTitle.innerHTML = "<h3>Hello</h3>";
-headerTitle.style.borderBottom = "solid 3px #000";
+//headerTitle.innerHTML = "<h3>Hello</h3>";
+//headerTitle.style.borderBottom = "solid 3px #000";
 
 //GETELEMENTBYCLASSNAME //
-var item = document.getElementsByClassName("list-group-item");
+/*var item = document.getElementsByClassName("list-group-item");
 console.log(item);
 console.log(item[1]);
 item[1].textContent = "Hello 2";
 item[1].style.fontWeight = "bold";
-item[1].style.backgroundColor = "yellow";
+item[1].style.backgroundColor = "yellow";*/
 
-var header = document.getElementsByClassName("title");
+//gives error
+//item.style.backgroundColor = "#f4f4f4";
 
-main.style.color = "green";
-main.style.fontWeight = "bold";
+//var header = document.getElementsByClassName("title");
+//main.style.color = "green";
+//main.style.fontWeight = "bold";
+
+/*for (var i = 0; i < item.length; i++) {
+  item[1].style.backgroundColor = "#f4f4f4";
+  item[2].style.backgroundColor = "green";
+  main.style.fontWeight = "bold";
+}*/
+
+//GETELEMENTBYTAGNAME //
+var li = document.getElementsByTagName("li");
+console.log(li);
+console.log(li[1]);
+li[1].textContent = "Hello 2";
+li[1].style.fontWeight = "bold";
+li[1].style.backgroundColor = "yellow";
+
+for (var i = 0; i < li.length; i++) {
+  li[i].style.backgroundColor = "#f4f4f4";
+}
+
+// QUERYSELECTOR //
+/*var header = document.querySelector("#main-header");
+header.getElementsByClassName.borderBottom = "solid 4px #ccc";
+
+var input = document.querySelector("input");
+input.value = "Hello world";
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value = "SEND";
+
+var item = document.querySelector(".list-group-item");
+item.style.color = "red";
+*/
